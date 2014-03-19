674989974cd701e45089d299f0ec382b6a25b92a
Martin Vejmelka <vejmelkam@gmail.com>
2014-03-19 13:06:53 -0600

    restructured how displays the trajectory (moved to analyze_dynamics), onet_integrate now only returns the trajectory and an (approximate) identifier of the equilibrium

--- opinion_networks/onet_analyze_dynamics.m
+++ opinion_networks/onet_analyze_dynamics.m
@@ -7,19 +7,27 @@
 %
 % synopsis: xie2012_analyze_dynamics(pa, pb,res)
 %
-function onet_analyze_dynamics(pa,pb,res)
+function fs = onet_analyze_dynamics(pa,pb,res)
     if(nargin < 3)
         res = 0.1;
     end
+    fmts = { 'r-', 'b-', 'g-' };
     onet_quiver(pa,pb,0.02);
     hold on
-    for na=0:res:1.0
-        for nb=0:res:1.0
+    rng = 0:res:1.0;
+    fs = zeros(length(rng)^2,2);
+    ndx = 1;
+    for na=rng
+        for nb=rng
             if(na+nb+pa+pb <= 1)
-                onet_integrate(pa,pb,na,nb,500);
+                [xy,eqid] = onet_integrate(pa,pb,na,nb,500);
+                plot(xy(:,1),xy(:,2), fmts{eqid}, 'linewidth', 1.5);
+                plot(xy(end,1),xy(end,2), 'ko', 'markersize', 8, 'markerfacecolor', 'k');
+                fs(ndx,:) = [xy(end,1),xy(end,2)];
+                ndx = ndx + 1;
             end
         end
     end
     hold off
-
+    fs = fs(1:ndx-1,:);
 end
--- opinion_networks/onet_integrate.m
+++ opinion_networks/onet_integrate.m
@@ -1,17 +1,27 @@
 
+%
+% Computes a trajectory of the mean field model starting from
+% the point (pa,pb,na,nb,1-pa-pb-na-nb).  Integration period is from 0 to
+% Tmax.
+%
+% The function additionally (in a very simple manner) classifies the
+% equilibrium toward which the trajectory converges as being either high
+% na/low nb (1), high nb/low na (3) or mid na/mid nb (2).
+%
+% synopsis: [xy,eqid] = onet_integrate(pa,pb,na,nb,Tmax)
+%
+%
 
-function onet_integrate(pa,pb,na,nb,Tmax)
+function [xy,eqid] = onet_integrate(pa,pb,na,nb,Tmax)
     traj = ode45(@(t,x) onet_model(pa,pb,x), [0,Tmax], [na,nb]);
     xy = traj.y';
-    if((abs(xy(end,1) -xy(end,2)) > 0.5 * (1 - pa - pb)))
+    if((abs(xy(end,1) -xy(end,2)) > 0.2 * (1 - pa - pb)))
         if(xy(end,1) > xy(end,2))
-            fmt = 'r-';
+            eqid = 1;
         else
-            fmt = 'g-';
+            eqid = 3;
         end
     else
-        fmt = 'b-';
+        eqid = 2;
     end
-    plot(xy(:,1),xy(:,2), fmt, 'linewidth', 1.5);
-    plot(xy(end,1),xy(end,2), 'ko', 'markersize', 8, 'markerfacecolor', 'k');
 end
